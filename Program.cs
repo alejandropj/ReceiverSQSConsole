6abb1d88c3ed365f29bfbf41fee09f4c44803c85
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-20 11:18:33 +0200

    20/05

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.SQS;
+using Microsoft.Extensions.DependencyInjection;
+using ReceiverSQSConsole;
+
+Console.WriteLine("Receiver SQS");
+var serviceProvider = new ServiceCollection()
+    .AddAWSService<IAmazonSQS>()
+    .AddTransient<ServiceSQS>()
+    .BuildServiceProvider();
+ServiceSQS service =
+    serviceProvider.GetService<ServiceSQS>();
+List<Mensaje> mensajes = await service.ReceiveMessagesAsync();
+if (mensajes == null)
+{
+    Console.WriteLine("No existen mensajes en Queue");
+}
+else
+{
+    Console.WriteLine("Numero de mensajes: " + mensajes.Count);
+    foreach (Mensaje msj in mensajes)
+    {
+        Console.WriteLine("------------START----------");
+        Console.WriteLine("Asunto: " + msj.Asunto);
+        Console.WriteLine("Email: " + msj.Email);
+        Console.WriteLine("Contenido: " + msj.Contenido);
+        Console.WriteLine("Fecha: " + msj.Fecha);
+        Console.WriteLine("------------END----------");
+    }
+    Console.WriteLine("Fin lectura mensajes");
+}
+Console.WriteLine("Fin de programa");
